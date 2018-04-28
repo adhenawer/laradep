9e6dce1707c04c611c1dbf5baabc7bfd452194a2
Rodolfo Moraes <adhenawer@gmail.com>
2018-04-28 17:30:33 -0300

    Add pipeline gitlab

--- deploy.php
+++ deploy.php
@@ -9,22 +9,11 @@ set('application', 'laradep');
 // Project repository
 set('repository', 'git@gitlab.com:adhenawer/laradep.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
-
-// Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
-
-// Writable dirs by web server 
-add('writable_dirs', []);
-
-
 // Hosts
 
 host('159.89.184.227')
     ->user('deployer')
-    ->identityFile('~/.ssh/deployer@app-digitalocean')
+    ->identityFile('~/.ssh/deployerkey')
     ->set('deploy_path', '/var/www/html/laradep');    
     
 // Tasks
@@ -33,10 +22,6 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
-// [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
-
 // Migrate database before symlink new release.
-
 #before('deploy:symlink', 'artisan:migrate');
 
